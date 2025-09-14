25069307278c09c84d4b64ab050079f7d519a5ac
ᴏɢᴄᴀᴇ <carloslurlin@gmail.com>
2025-09-14 23:15:47 +0200

    🎉 v1.0.2

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,9 @@
+#| ------------------------------------------ |
+__email__   =  "c.ogcae@engineer.com"
+__name__    =  "cryptoforge"
+__version__ =  "1.0.2"
+__author__  =  "ogcae"
+#| ------------------------------------------ |
+
 from .keypair import RSAKeyPair
 __all__ = ['RSAKeyPair']
