d047ba7921ff0e40d092fa439ea8c515d66a87bb
ᴏɢᴄᴀᴇ <carloslurlin@gmail.com>
2025-09-14 23:17:49 +0200

    🎉 v1.0.2

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,2 +1,9 @@
+#| ------------------------------------------ |
+__email__   =  "c.ogcae@engineer.com"
+__name__    =  "cryptoforge"
+__version__ =  "1.0.2"
+__author__  =  "ogcae"
+#| ------------------------------------------ |
+
 from .rsa import RSAEngine
 __all__ = ['RSAEngine']
